1d927dd71267829af1ea3acd28e82f3c3e4d8cd1
Juliana Ramos <juramos266@gmail.com>
2022-11-28 02:41:44 -0300

    Update APIWEB

---
+++ ApiPizzaria/Controllers/PizzaController.cs
@@ -0,0 +1,62 @@
+﻿using ApiPizzaria.Models;
+using ApiPizzaria.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ApiPizzaria.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+
+        }
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if(pizza == null)
+                return NotFound();
+            return Ok(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if(id != pizza.Id)
+                return BadRequest();
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+            PizzaService.Update(existingPizza);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza is null)
+                return NotFound();
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ ApiPizzaria/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiPizzaria.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public bool ESemGluten { get; set; }
+
+    }
+}
